00d4a192056f34cf700a26565db0284d0be047cb
taishi <taiyam27@bergen.org>
2025-09-15 10:25:46 -0400

    l14

--- Lesson_04_Routes/app.py
+++ Lesson_04_Routes/app.py
@@ -10,17 +10,12 @@ def home():
 <ul>
     <li><a href="/user/john">User Profile: john</a></li>
     <li><a href="/user/alice">User Profile: alice</a></li>
-    <li><a href=""></a>User Profile:</li>
-    <li><a href=""></a>User Profile:</li>
-    <li><a href=""></a>User Profile:</li>
-    <li><a href=""></a>User Profile:</li>
-    <li><a href=""></a>User Profile:</li>
 </ul>
 '''
 
 @app.route('/user/<username>', methods=['GET'])
-def user_profile():
- return """
+def user_profile(username):
+ return f"""
     <h1>User Profile</h1>
 <p>Username: <strong>{username}</strong></p>
 <p>Profile Type: {type(username).__name__}</p>
@@ -31,7 +26,21 @@ def user_profile():
     <a href="/user/bob">Bob</a>
 </nav>
 """
-
+@app.route("/calc/<int:num1>/<operation>/<int:num2>")
+def calculator(num1, operation, num2):
+    operations = {
+      '+': num1 + num2,
+      '-': num1-num2,
+      '*': num1 * num2,
+      '/': num1 / num2 if num2 != 0 else 'Error: Division by 0'
+    }
+    if operation in operations:
+      result = operations[operation]
+      return f"{num1} {operation} {num2} = {result}"
+    else: 
+        return f"unknown operation {operation}"
+      
+ 
 if __name__ == '__main__':
  app.run(debug=True)
 
