a2a7bfb3d6646caba5bdc06919ab0c55c27c56cd
taishi <taiyam27@bergen.org>
2025-09-22 10:20:25 -0400

    Template Rendering

---
+++ Lesson_05_Templates_rendering/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+#  return 'Hello, World!'
+ return render_template("home.html")
+
+@app.route('/profile/<username>')
+def profile(username):
+ user_data = {
+  'name:' : username,
+  'school': 'BT',
+  'age': 16,
+  'is_present': True
+ }
+ return render_template("profile.html", user=user_data) 
+if __name__ == '__main__':
+ app.run(debug=True)
